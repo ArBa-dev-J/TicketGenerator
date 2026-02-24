a3c99b5e7226a84cd497a6de7e52b2d7e92eaa90
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-02-24 13:51:44 +0200

    add basic routong for front

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,14 @@
+import { Routes, Route } from "react-router";
 
 function App() {
-  
-
   return (
     <>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/ticket" element={<Ticket />} /> */}
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
