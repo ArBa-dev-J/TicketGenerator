fcd0eba4d17efaac44357d8f6355d042d49ca41e
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-02-24 13:38:16 +0200

    add cors

--- back/app.js
+++ back/app.js
@@ -1,8 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
 import atendeeRoutes from "./router/atendeeRoutes.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: `http://localhost:5173`,
+    credentials: false,
+  }),
+);
 
 // create application/json parser
 const jsonParser = bodyParser.json();
