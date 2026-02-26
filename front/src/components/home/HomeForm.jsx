c41d566e7d95ee267ac298aa3d70ca6efb0612fc
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-02-26 14:08:48 +0200

    add basic data register

--- front/src/components/home/HomeForm.jsx
+++ front/src/components/home/HomeForm.jsx
@@ -1,36 +1,67 @@
+import { useForm } from "react-hook-form";
+
 function HomeForm() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      avatar: "",
+      emailAddress: "",
+      password: "",
+      githubUsername: "",
+    },
+  });
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    reset();
+  };
+
   return (
     <>
-        <form className="flex flex-col items-center">
-          <label className="block">Avatar</label>
-          <input
-            type="text"
-            className="border"
-            placeholder="Put img url here"
-          />
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col items-center"
+      >
+        <label className="block">Avatar</label>
+        <input
+          type="text"
+          {...register("avatar", { required: true })}
+          className="border"
+          placeholder="Put img url here"
+        />
 
-          <label className="block">Full name</label>
-          <input type="text" className="border" />
+        <label className="block">Full name</label>
+        <input type="text" className="border" />
 
-          <label className="block">Email Address</label>
-          <input
-            type="email"
-            className="border"
-            placeholder="example@email.com"
-          />
+        <label className="block">Email Address</label>
+        <input
+          type="email"
+          {...register("emailAddress", { required: true })}
+          className="border"
+          placeholder="example@email.com"
+        />
 
-          <label className="block">Password</label>
-          <input type="password" className="border" />
+        <label className="block">Password</label>
+        <input
+          type="password"
+          {...register("password", { required: true })}
+          className="border"
+        />
 
-          <label className="block">Github username</label>
-          <input
-            type="password"
-            className="border"
-            placeholder="@yourusername"
-          />
+        <label className="block">Github username</label>
+        <input
+          type="text"
+          {...register("githubUsername", { required: true })}
+          className="border"
+          placeholder="@yourusername"
+        />
 
-          <input type="submit" className="border" value="Sign up" />
-        </form>
+        <input type="submit" className="border" value="Sign up" />
+      </form>
     </>
   );
 }
