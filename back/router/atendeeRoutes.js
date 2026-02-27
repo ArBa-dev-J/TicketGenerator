8617f4f5acf639d29404472067efa07fa57d89e7
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-02-27 09:30:35 +0200

    add validation or login

--- back/router/atendeeRoutes.js
+++ back/router/atendeeRoutes.js
@@ -10,12 +10,13 @@ import {
 } from "../controller/atendeeControllers.js";
 import validate from "../validate/validate.js";
 import validateNewData from "../validate/validateNewData.js";
+import validateLogin from "../validate/validateLogin.js";
 
 const atendeeRoutes = express.Router();
 
 atendeeRoutes.route("/logout").get(protect, logout);
 atendeeRoutes.route("/signup").post(validateNewData, validate, postNewDataC);
-atendeeRoutes.route("/login").post(login);
+atendeeRoutes.route("/login").post(validateLogin, validate, login);
 atendeeRoutes
   .route("/:id")
   .get(protect, allowAccessTo("user"), getAuthenticatedUser, getByIdC);
